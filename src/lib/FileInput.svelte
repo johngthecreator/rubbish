5311bc691e917839490b6b8bbe84f60a9ae5085d
John Gorriceta <79299314+jongee0w0@users.noreply.github.com>
2023-01-29 21:27:23 -0700

    some b64 hints

---
+++ src/lib/FileInput.svelte
@@ -0,0 +1,25 @@
+<script>
+let files;
+let base64;
+function getBase64() {
+   var reader = new FileReader();
+   reader.readAsDataURL(files[0]);
+   reader.onload = function () {
+     base64 = reader.result;
+   };
+   reader.onerror = function (error) {
+     console.log('Error: ', error);
+   };
+}
+
+const readFile = () => {
+    var reader = new FileReader();
+    reader.readAsDataURL(base64);
+}
+
+
+
+</script>
+<input bind:files type="file" />
+<button on:click={getBase64}>Log File</button>
+<button on:click={readFile}>Read File</button>
