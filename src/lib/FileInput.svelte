52c1729d1047439278468398701f656643e48590
John Gorriceta <79299314+jongee0w0@users.noreply.github.com>
2023-04-15 19:17:03 -0600

    file download

--- src/lib/FileInput.svelte
+++ src/lib/FileInput.svelte
@@ -6,20 +6,15 @@ function getBase64() {
    reader.readAsDataURL(files[0]);
    reader.onload = function () {
      base64 = reader.result;
+     console.log(reader.result);
    };
    reader.onerror = function (error) {
      console.log('Error: ', error);
    };
 }
 
-const readFile = () => {
-    var reader = new FileReader();
-    reader.readAsDataURL(base64);
-}
-
-
 
 </script>
 <input bind:files type="file" />
 <button on:click={getBase64}>Log File</button>
-<button on:click={readFile}>Read File</button>
+<a href="{base64}" download="test.jpg">Download</a>
